d0972a229a19f75defe0052c74ef5e74edf696b1
SyspreeDev <development@syspreedigital.com>
2025-11-25 10:35:32 +0530

    test

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -7,7 +7,7 @@ export default function Navbar() {
 
   const navLinks = [
     { name: "Home", href: "/" },
-    { name: "Training", href: "/" },
+    { name: "Training", href: "/training"},
     { name: "About us", href: "/about-us" },
     { name: "Products", href: "/products" },
     { name: "Contact", href: "/contact" },
