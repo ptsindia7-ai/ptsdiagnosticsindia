4a71005730db859fc62decfa0cb5b1f0aeb91ae8
SyspreeDev <development@syspreedigital.com>
2025-12-02 11:41:48 +0530

    added heading for product page

--- app/products/page.js
+++ app/products/page.js
@@ -7,7 +7,7 @@ export default function ProductCards() {
       name: "ICare- 2100",
       sub: "Portable Automatic Multi-function Analyzer",
       img: "/products/p11.png",
-      hoverImg: "/products/p1-hover.png", 
+      hoverImg: "/products/p1-hover.png",
     },
     {
       name: "ican-I6",
@@ -79,6 +79,23 @@ export default function ProductCards() {
 
   return (
     <>
+      <section className="w-full py-16 px-6 md:px-12">
+        <div className="max-w-7xl mx-auto flex flex-col md:flex-row items-start md:items-center justify-between gap-6">
+          {/* Left Heading */}
+          <h2 className="text-4xl md:text-5xl font-semibold leading-tight text-gray-900">
+            Our Products Across <br /> Healthcare Categories
+          </h2>
+
+          {/* Right Button */}
+          <a
+            href="/products"
+            className="inline-block bg-[#DF1931] text-white text-sm md:text-base font-medium px-6 py-3 rounded-xl shadow-md hover:bg-[#c5162a] transition duration-300"
+          >
+            Explore Our Products
+          </a>
+        </div>
+      </section>
+      
       <section className="py-14 px-6 lg:px-20">
         <div className="max-w-7xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-14">
           {products.map((product, index) => (
