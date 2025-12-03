77431efaf377b77dea441bf8f93af9bf72909605
SyspreeDev <development@syspreedigital.com>
2025-12-03 10:59:15 +0530

    Added blank videos for training page

--- app/data/trainingData.js
+++ app/data/trainingData.js
@@ -2,7 +2,7 @@ export const trainingData = {
   ican3: {
     type: "video",
     title: "iCan-3",
-    videoType: "file", // 
+    videoType: "file", //
     videoUrl: "/videos/ican3.mp4",
     // videoId: "zpOULjyy-n8", // extract only ID from YouTube embed
     rating: 4.5,
@@ -28,8 +28,20 @@ export const trainingData = {
   },
 
   a1cnow: {
-    type: "pdf",
+    type: "video",
     title: "A1CNOW+",
-    pdf: "/pdfs/analyzer-pro.pdf",
+    videoType: "file", //
+    videoUrl: "/videos/ a1cnow.mp4",
+    // videoId: "zpOULjyy-n8", // extract only ID from YouTube embed
+    rating: 4.5,
+    reviews: 251,
+    description:
+      "We are a global leader in innovative point-of-care diagnostic devices...",
+    steps: [
+      { title: "Demonstration", time: "00:00", start: 0 },
+      { title: "Blood Collector", time: "00:15", start: 15 },
+      { title: "Running the test", time: "02:00", start: 120 },
+      { title: "End", time: "03:32", start: 213 },
+    ],
   },
 };
