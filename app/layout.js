3edd616e14972997bf4e862554fab40757f89ba8
syspree <syspreevimeoid@gmail.com>
2026-07-17 17:32:34 +0530

    GA code Added

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import "./globals.css";
 import { Geist, Geist_Mono, Manrope} from "next/font/google";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Script from "next/script";
 
 
 
@@ -28,10 +29,26 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+        <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-PNCK8PBH02"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-PNCK8PBH02');
+          `}
+        </Script>
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable} ${manrope.variable}  antialiased`}>
         <Navbar />
         {children}
+        
         <Footer />
+        
       </body>
     </html>
   );
